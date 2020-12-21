7266e09f01bc71ab058f7d2b82ebc9fd0b0eaec7
Alan <freebirdphl@gmail.com>
2020-12-21 21:19:25 +0800

    draw bird on surface view

--- app/src/main/java/top/alanpu/android/flappybird/GameView.kt
+++ app/src/main/java/top/alanpu/android/flappybird/GameView.kt
@@ -2,12 +2,9 @@ package top.alanpu.android.flappybird
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import android.view.View
 
 /**
  * Custom view to draw the game area.
@@ -16,8 +13,10 @@ class GameView : SurfaceView, Runnable, SurfaceHolder.Callback {
 
     private lateinit var bmBird: Bitmap
     private lateinit var gameThread: Thread
-    private lateinit var surfaceHolder: SurfaceHolder
     private var running = false
+
+    private var birdPosLeft = 0f
+    private var birdPosTop = 0f
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -39,40 +38,22 @@ class GameView : SurfaceView, Runnable, SurfaceHolder.Callback {
         setZOrderOnTop(true)
         keepScreenOn = true
         bmBird = BitmapFactory.decodeResource(resources, R.drawable.ic_bird)
-        surfaceHolder = holder
-        surfaceHolder.addCallback(this)
+        holder.addCallback(this)
+        holder.setFormat(PixelFormat.TRANSPARENT)
     }
 
     fun resume() {
-        running = true
-        gameThread = Thread(this)
-        gameThread.start()
+        startGame()
     }
 
     fun pause() {
-        running = false
-        try {
-            gameThread.join()
-        }
-        catch (e: InterruptedException) {
-
-        }
+        stopGame()
     }
 
     override fun run() {
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        var canvas: Canvas
-
-        println("******* In run(), ${holder.surface.isValid}")
         while (running) {
-            if (holder.surface.isValid) {
-                canvas = surfaceHolder.lockCanvas()
-                canvas.save()
-                canvas.drawBitmap(bmBird, 0f, 0f, paint)
-                canvas.drawRect(0f, 0f, 100f, 100f, paint)
-                canvas.restore()
-                holder.unlockCanvasAndPost(canvas)
-            }
+            birdPosTop += 5
+            draw()
         }
 
 //        with(holder) {
@@ -86,15 +67,44 @@ class GameView : SurfaceView, Runnable, SurfaceHolder.Callback {
 //        }
     }
 
+    private fun draw() {
+        if (holder.surface.isValid) {
+            holder.apply {
+                this.lockCanvas()?.let {
+                    // Clear the canvas
+                    it.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+
+                    it.save()
+                    it.drawBitmap(bmBird, birdPosLeft, birdPosTop, null)
+                    it.restore()
+                    this.unlockCanvasAndPost(it)
+                }
+            }
+        }
+    }
+
+    private fun stopGame() {
+        running = false
+        try {
+            gameThread.join()
+        } catch (e: InterruptedException) {
+        }
+    }
+
+    private fun startGame() {
+        running = true
+        gameThread = Thread(this)
+        gameThread.start()
+    }
+
     override fun surfaceCreated(holder: SurfaceHolder) {
-        println("******* Surface created, ${holder.surface.isValid}")
+        startGame()
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        println("******* Surface changed, ${holder.surface.isValid}")
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
-        println("******* Surface destroyed, ${holder.surface.isValid}")
+        stopGame()
     }
 }
